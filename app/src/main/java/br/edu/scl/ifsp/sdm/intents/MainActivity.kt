ed03d696881e1b3e3ec0063089cb8443a78eab82
Fernando <suporteiros@gmail.com>
2024-09-24 18:36:50 -0300

    MainActivity com registerForActivityResult

--- app/src/main/java/br/edu/scl/ifsp/sdm/intents/MainActivity.kt
+++ app/src/main/java/br/edu/scl/ifsp/sdm/intents/MainActivity.kt
@@ -1,9 +1,14 @@
 package br.edu.scl.ifsp.sdm.intents
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
+import br.edu.scl.ifsp.sdm.intents.Extras.PARAMETER_EXTRA
 import br.edu.scl.ifsp.sdm.intents.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,17 +16,50 @@ class MainActivity : AppCompatActivity() {
         ActivityMainBinding.inflate(layoutInflater)
     }
 
+    /* Bloco não mais necessário pois agora implementamos o método registerForActivityResult
+    companion object{
+        private const val PAREMETER_REQUEST_CODE = 0
+    }*/
+
+    private lateinit var parameterArl: ActivityResultLauncher<Intent>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(activityMainBinding.root)
         setSupportActionBar(activityMainBinding.toolbarIn.toolbar)
         supportActionBar?.subtitle = localClassName
 
-        activityMainBinding.parameterBt.setOnClickListener {
+        parameterArl = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == RESULT_OK) {
+                result.data?.getStringExtra(PARAMETER_EXTRA)?.also {
+                    activityMainBinding.parameterTv.text = it
+                }
+            }
+        }
 
+        activityMainBinding.apply{
+            parameterBt.setOnClickListener {
+                val parameterIntent = Intent(this@MainActivity, ParameterActivity::class.java).apply {
+                    putExtra(PARAMETER_EXTRA, parameterTv.text)
+                }
+                parameterArl.launch(parameterIntent)
+                //startActivityForResult(parameterIntent, PAREMETER_REQUEST_CODE) //startActivityForResult deprecated
+            }
         }
     }
 
+/* Bloco não mais necessário pois agora implementamos o método registerForActivityResult
+
+    @Deprecated("Deprecated in Java")
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == PAREMETER_REQUEST_CODE && resultCode == RESULT_OK){
+            data?.getStringExtra(PARAMETER_EXTRA)?.also {
+                activityMainBinding.parameterTv.text = it
+            }
+        }
+    }
+*/
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.menu_main, menu)
         return true
--- app/src/main/java/br/edu/scl/ifsp/sdm/intents/ParameterActivity.kt
+++ app/src/main/java/br/edu/scl/ifsp/sdm/intents/ParameterActivity.kt
@@ -2,20 +2,34 @@ package br.edu.scl.ifsp.sdm.intents
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import br.edu.scl.ifsp.sdm.intents.Extras.PARAMETER_EXTRA
 import br.edu.scl.ifsp.sdm.intents.databinding.ActivityParameterBinding
 
 class ParameterActivity : AppCompatActivity() {
     private val activityParameterBinding: ActivityParameterBinding by lazy {
         ActivityParameterBinding.inflate(layoutInflater)
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(activityParameterBinding.root)
         setSupportActionBar(activityParameterBinding.toolbarIn.toolbar)
         supportActionBar?.subtitle = localClassName
 
-        activityParameterBinding.returnCloseBt.setOnClickListener{
-
+        intent.getStringExtra(Extras.PARAMETER_EXTRA)?.let {
+            activityParameterBinding.parameterEt.setText(it)
         }
+
+        activityParameterBinding.apply {
+            returnCloseBt.setOnClickListener {
+                val resulIntent = intent.apply {
+                    putExtra(PARAMETER_EXTRA, parameterEt.text.toString())
+                }
+                //Valor result é um valor padrão de Activity.java
+                setResult(RESULT_OK, resulIntent)
+                finish()
+            }
+        }
+
     }
 }
